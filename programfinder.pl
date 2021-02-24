1fa4af315659ae60b0f959f6a076bfb47dabdeb3
Lucian Green <luciangreen@lucianacademy.com>
2021-02-24 23:45:33 +1100

    Update programfinder.pl

--- programfinder.pl
+++ programfinder.pl
@@ -1,11 +1,8 @@
-:- include('../listprologinterpreter/grammar.pl').
-:- include('../listprologinterpreter/listprologinterpreter1listrecursion4.pl').
-:- include('../listprologinterpreter/listprologinterpreter3preds5.pl').
-:- include('../listprologinterpreter/lpiverify4.pl').
+:- include('../listprologinterpreter/listprolog.pl').
 %%:- include('caw5 copy 12.pl').
 %%:- include('cawpverify.pl').
 %%:- include('rcawp.pl').
 %%:- include('rcaw.pl').
 %%**:- include('texttobr2.pl').
-:- include('../listprologinterpreter/la_strings.pl').
+%:- include('../listprologinterpreter/la_strings.pl').
 :- include('program_finder.pl').
