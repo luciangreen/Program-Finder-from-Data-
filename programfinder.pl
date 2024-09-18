459557a0ff46e68cb7d7b8f4a404ebe1fc91c6f6
Lucian Green <luciangreen@lucianacademy.com>
2024-09-18 18:26:44 +1000

    Deleted space

--- programfinder.pl
+++ programfinder.pl
@@ -1,8 +1,8 @@
-:- include('../listprologinterpreter/listprolog.pl').
-%%:- include('caw5 copy 12.pl').
-%%:- include('cawpverify.pl').
-%%:- include('rcawp.pl').
-%%:- include('rcaw.pl').
-%%**:- include('texttobr2.pl').
-%:- include('../listprologinterpreter/la_strings.pl').
-:- include('program_finder.pl').
+:-include('../listprologinterpreter/listprolog.pl').
+%%:-include('caw5 copy 12.pl').
+%%:-include('cawpverify.pl').
+%%:-include('rcawp.pl').
+%%:-include('rcaw.pl').
+%%**:-include('texttobr2.pl').
+%:-include('../listprologinterpreter/la_strings.pl').
+:-include('program_finder.pl').
